168c60bd03d425ac53fa0f50d9886279e9d08519
ManzhiFu <412378709@qq.com>
2020-01-08 16:25:02 +0800

    修改sass配置方式

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        data: `@import "@/styles/_variable.scss";`
+        prependData: `@import "~@/styles/_variable.scss";`
       }
     }
   }
