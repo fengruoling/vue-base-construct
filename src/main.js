9be3b2584efd8a68110aae914eb6937d44a98a5e
ManzhiFu <412378709@qq.com>
2020-01-08 16:41:21 +0800

    引入样式文件

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'amfe-flexible' // 根字体设置
+import '@/styles/index.scss'
 
 Vue.config.productionTip = false
 
